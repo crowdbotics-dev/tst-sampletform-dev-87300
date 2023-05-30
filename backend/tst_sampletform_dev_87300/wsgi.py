71916a5b637c10bf59e8ac663e574edc94f5a7eb
Crowdbotics <engineering@crowdbotics.com>
2023-05-30 19:14:08 +0000

    Initial commit.

---
+++ backend/tst_sampletform_dev_87300/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tst_sampletform_dev_87300 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tst_sampletform_dev_87300.settings')
+
+application = get_wsgi_application()
